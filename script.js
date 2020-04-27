7281194fab679a26ab09cd9314a3d1b645a5057a
Videl90 <lorena.sa.0117@gmail.com>
2020-04-27 02:58:15 -0500

    added all quiz questions and answers as objets in an array

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-//All the elements//
-var time = document.querySelector(".time");
-var seconds = document.getElementById("seconds");
-var question = document.getElementById("question"); 
+//Set the body to a variable
+var body = document.body;
+
+//Set the main container to a variable
+var mainEl = document.querySelector(".card-body");
+var ques = document.querySelector(".question");
+var answerEl = document.querySelector(".list-group list-group-flush");
 var choiceA = document.getElementById("button-A");
 var choiceB = document.getElementById("button-B");
 var choiceC = document.getElementById("button-C");
 var choiceD = document.getElementById("button-D");
+var answer = document.getElementById("answer");
 
-//question buttons in a variable//
+//All the elements//
+var time = document.querySelector(".time");
+var seconds = document.getElementById("seconds");
 
-var answerOptions = document.getElementsByTagName("question");
-
-//Here are the timer functions//
-var timeLeft = 10;
+var timeLeft = 100;
 
 function setTime() {
     var timerRun = setInterval(function() {
@@ -33,100 +36,145 @@ function sendMessage() {
 
 setTime();
 
-//Questions Arrays//
+//Set all my questions as objets inside and array
 
-var questions = [
- {
-     question: "1. How many species of sharks exist?",
-     choiceA: "a.465",
-     choiceB: "b.360",
-     choiceC: "c.530",
-     coiceD: "d.250",
-     correct: "A"
- },
+var allQuestions = [
+    {
+        question: "1.How many species of sharks exist?",
+        choiceA: "a.465",
+        choiceB: "b.360",
+        choiceC: "c.530",
+        choiceD: "d.250",
+        correct: choiceA,
+    },
+   
+    {
+       question: "2.For how long have sharks existed on earth?",
+       choiceA: "a.200 thousands of years",
+       choiceB: "b.50 millions of years",
+       choiceC: "c.450 millions of years",
+       choiceD: "d.5 millions of years",
+       correct: choiceC,
+   },
+   
+   {
+       question: "3. Which is the biggest shark specie?",
+       choiceA: "a.Lemon Shark",
+       choiceB: "b.Whale Shark",
+       choiceC: "c.Great White Shark",
+       choiceD: "d.Tigger Shark",
+       correct: choiceB,
+   },
+   
+   {
+       question: "4. Which shark specie is able to swim in fresh water?",
+       choiceA: "a. Hammerhead Shark",
+       choiceB: "b. Bull Shark",
+       choiceC: "c. Mako Shark",
+       choiceD: "d. Angel Shark",
+       correct: choiceB,
+   },
+   
+   {
+       question: "5. Carcharodon Carcharias is the scientific name for:",
+       choiceA: "a. Blacktip Rees Shark",
+       choiceB: "b. Basking Shark",
+       choiceC: "c. Blue Shark",
+       choiceD: "d. Great White Shark",
+       correct: choiceD,
+   },
+       
+   {
+       question: "6. The Carcharodon Megalodon, is an extinct specie. It was the prehistoric sibiling to the Great White Shark. ¿Which do you thing was its length?",
+       choiceA: "a. 3-5 mts.",
+       choiceB: "b. 6-10 mts.",
+       choiceC: "c. 12-15 mts.",
+       choiceD: "d. 15-18 mts.",
+       correct: choiceD,
+   },
+   
+   {
+       question: "7. The Spiny dogfish is the longest-living shark specie. How many years do you think it lives?",
+       choiceA: "a. 100-150 years",
+       choiceB: "b. 20-40 years",
+       choiceC: "c. 70-100 years",
+       coiceD: "d. 10-20 years",
+       correct: choiceC,
+   },
+   
+   {
+       question: "8. What is the Ampullae of Lorenzini?",
+       choiceA: "a. Ampullae... WHAT?",
+       choiceB: "b. What give sharks the super power of smelling blood kms away",
+       choiceC: "c. Special sensing organs that help sharks and other species to sense electric fields in the water.",
+       choiceD: "d. An alternative name to sharks dorsal fin ",
+       correct: choiceC,
+       
+   },
+   
+   {
+       question: "9. How many teeth a shark looses in a year?",
+       choiceA: "a. 1000",
+       choiceB: "b. 250",
+       choiceC: "c. 500",
+       choiceD: "d. 100",
+       correct: choiceA,
+   },
+   
+   {
+       question: "10. Are sharks dangerous to humans?",
+       choiceA: "a. Sure. They eat humans.",
+       choiceB: "b. Not at all. Humans are such much more dangerous for sharks",
+       choiceC: "As dangerous as Coronavirus ",
+       choiceD: "Sure, they are as scarier as my ex",
+       correct: choiceB,
+   }, 
+   ]
 
- {
-    question: "2.For how long have sharks existed on earth?",
-    choiceA: "a.200 thousands of years",
-    choiceB: "b.50 millions of years",
-    choiceC: "c.450 millions of years.",
-    coiceD: "d.5 millions of years.",
-    correct: "C"
-},
 
-{
-    question: "3. Which is the biggest shark specie?",
-    choiceA: "a.Lemon Shark",
-    choiceB: "b.Whale Shark",
-    choiceC: "c.Great White Shark",
-    coiceD: "d.Tigger Shark",
-    correct: "B"
-},
+//Set text content 
 
-{
-    question: "4. Which shark specie is able to swim in fresh water?",
-    choiceA: "a. Hammerhead Shark",
-    choiceB: "b. Bull Shark",
-    choiceC: "c. Mako Shark",
-    coiceD: "d. Angel Shark",
-    correct: "A"
-},
+ques.textContent = allQuestions[i].question;
+choiceA.textContent = allQuestions[i].choiceA;
+choiceB.textContent = allQuestions[i].choiceB;
+choiceC.textContent = allQuestions[i].choiceC;
+choiceD.textContent = allQuestions[i].choiceD;
+answer.textContent = allQuestions[i].correct;
 
-{
-    question: "5. Carcharodon Carcharias is the scientific name for:",
-    choiceA: "a. Great White Shark",
-    choiceB: "b. Basking Shark",
-    choiceC: "c. Blue Shark",
-    coiceD: "d. Blacktip Rees Shark",
-    correct: "A"
-},
-    
-{
-    question: "6. The Carcharodon Megalodon, is an extinct specie. It was the prehistoric sibiling to the Great White Shark. ¿Which do you thing was its length?",
-    choiceA: "a. 3-5 mts.",
-    choiceB: "b. 6-10 mts.",
-    choiceC: "c. 12-15 mts.",
-    coiceD: "d. 15-18 mts.",
-    correct: "A"
-},
+//Apend elements//
 
-{
-    question: "7. The Spiny dogfish is the longest-living shark specie. How many years do you think it lives?",
-    choiceA: "a. 100-150 years.",
-    choiceB: "b. 20-40 years.",
-    choiceC: "c. 10-20 years.",
-    coiceD: "d. 70-100 years.",
-    correct: "A"
-},
+mainEl.appendChild(ques);
+mainEl.appendChild(answerEl);
 
-{
-    question: "8. What is the Ampullae of Lorenzini?",
-    choiceA: "a. Special sensing organs that help sharks and other species to sense electric fields in the water.",
-    choiceB: "b. What give sharks the super power of smelling blood kms away",
-    choiceC: "c. Ampullae... WHAT?",
-    coiceD: "d. An alternative name to sharks dorsal fin ",
-    correct: "A"
-},
+//questions loop //
 
-{
-    question: "9. How many teeth a shark looses in a year?",
-    choiceA: "a. 100",
-    choiceB: "b. 250",
-    choiceC: "c. 500",
-    coiceD: "d. 1000",
-    correct: "A"
-},
+for ( var i=0; i < allQuestions.length; i++){
+    var response = addEventListener("click", function(){
+        
+    })
+}
+   
+//Answers Text Content//
 
-{
-    question: "10. Are sharks dangerous to humans?",
-    choiceA: "a. Sure. They eat humans.",
-    choiceB: "b. Not at all. Humans are such much more dangerous for sharks",
-    choiceC: "As dangerous as Coronavirus ",
-    coiceD: "Sure, they are as scarier as my ex",
-    correct: "A"
-},
-    
-]
+correct.textContent = "You are awesome";
+incorrect.textContent = "Nice try :)";
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+   
+
+
+
 
 
 
